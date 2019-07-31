5faac5e5b80c7062c3e8400ad1fb062b1edb38d1
gonearewe <cristmactavish@outlook.com>
2019-07-31 15:20:31 +0800

    finish struct , developing PrintFile

--- article.go
+++ article.go
@@ -1,9 +1,48 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"time"
+)
+
 type Article struct {
 	title        string
 	subtitle     string
-	time         string
+	date         string
+	picture_name string
 	picture_tags []string
 	article_tags []string
+}
+
+func getInput(hint string) string {
+	var input string
+	for {
+		fmt.Print(hint)
+		fmt.Scanln(&input)
+		if input != "" {
+			break
+		}
+	}
+	return input
+
+}
+func getTags(hint string) []string {
+	str := getInput(hint)
+	println(str)
+	return strings.Split(str, ",")
+}
+func (it *Article) GetInfo() {
+	it.date = time.Now().Format("2006-01-02")
+	it.picture_tags = getTags("请输入图片标签（以逗号分隔）:")
+	fmt.Println(it.picture_tags)
+	it.title = getInput("请输入文章标题 :")
+	it.subtitle = getInput("请输入文章副标题 :")
+	it.article_tags = getTags("请输入文章标签（以逗号分隔）:")
+}
+func (it *Article) PrintFile() {
+	fileaddr := strings.Join([]string{ARTICLE_PATH, it.date, "-", it.title,".md" }, "")
+	ioutil.WriteFile(fileaddr, []byte(CONTENT), 0644)
+
 }
---
+++ config.go
@@ -0,0 +1,38 @@
+package main
+
+const ARTICLE_PATH="/mnt/d/MyProjects/gonearewe.github.io/_posts/"
+const PICTURE_PATH="/mnt/d/MyProjects/gonearewe.github.io/img/"
+const CONTENT=`
+---
+layout:     post
+title:      TITLE
+subtitle:   SUBTITLE
+date:       DATE
+author:     John Mactavish
+header-img: img/PICTURE_NAME
+catalog: true
+tags:
+    - ARTICLE_TAG
+    - ARTICLE_TAG
+    - ARTICLE_TAG
+---
+# 前言
+&emsp;&emsp;
+
+
+
+
+
+
+
+# 参考
+>[title](https://path)  
+>[title](https://path)    
+
+***  
+>最后附上GitHub：<https://github.com/gonearewe>
+`
+
+
+
+
